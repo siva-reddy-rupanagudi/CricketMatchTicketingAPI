79182a62b8e7f73ffc82d451794dd154482fe556
Siva Reddy Rupanagudi <srupanagudi@belenits.in>
2026-06-26 16:58:49 +0530

    changes done in the mapping

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM eclipse-temurin:21-jdk
 LABEL maintainer="SivaReddy"
 WORKDIR /app
-COPY target/rest-demo.jar app.jar
+COPY target/CricketMatchTicketingAPI.jar app.jar
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","app.jar"]
--- src/main/java/com/belenits/cricketmatchticketingapi/dto/MatcheDto.java
+++ src/main/java/com/belenits/cricketmatchticketingapi/dto/MatcheDto.java
@@ -1,12 +1,10 @@
 package com.belenits.cricketmatchticketingapi.dto;
 
-import com.belenits.cricketmatchticketingapi.entity.Tickets;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
